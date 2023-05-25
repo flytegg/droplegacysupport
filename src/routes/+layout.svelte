f74ee14feb3f5b537b0c374adac4d7fdfbe6a43f
sttephen <stephenatcollege@Gmail.com>
2023-05-25 08:39:11 +0100

    Discord embed updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <svelte:head>
-    <meta property="og:title" content="Site Title" />
+    <meta property="og:title" content="Drop Legacy Support" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="http://my.site.com" />
-    <meta property="og:image" content="stats.png" />
+    <meta property="og:url" content="https://droplegacy.support/" />
+    <meta property="og:image" content="https://i.imgur.com/bba2Ksm.png" />
     <meta name="twitter:card" content="summary_large_image">
     <meta property="og:description" content="Embrace progress, shed complexity, and thrive in the ecosystem— join the movement and drop Minecraft legacy version support to ignite innovation." />
     <meta name="theme-color" content="#2b2d31">
